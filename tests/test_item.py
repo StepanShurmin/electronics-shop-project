3195a5634687cabaa1ad10c8635595ba42c107f0
StepanShurmin <shurmin.sa@gmail.com>
2023-07-01 11:09:49 +0400

    Выполнил задание.

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,21 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_calculate_total_price():
+    item1 = Item("Смартфон", 10000, 20)
+    assert item1.calculate_total_price() == 200000
+
+
+def test_apply_discount():
+    item1 = Item("Смартфон", 10000, 20)
+    item1.apply_discount()
+    assert item1.price == 10000.0
+
+
+def test_all_items():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Ноутбук", 20000, 5)
+
+    assert item1 in Item.all
+    assert item2 in Item.all
